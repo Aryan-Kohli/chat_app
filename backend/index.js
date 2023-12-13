7ea78ee0e066dd789c17b5bac636f3eccfca9626
Aryan-Kohli <106698463+Aryan-Kohli@users.noreply.github.com>
2023-12-13 23:31:54 +0530

    done locally

---
+++ backend/index.js
@@ -0,0 +1,35 @@
+const io = require("socket.io")(8000, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"]
+  }
+});
+const users ={}
+
+
+io.on('connection', socket =>{ 
+    
+    socket.on('new-user-joined',name=>{
+        console.log(name , " has joined the chat .");
+        // it states that a particular connection bana hai, then hum uska name use kr rhe hai // the name 'new-user-joined' is a custom name not predefined
+        users[socket.id]=name;
+        var length = Object.keys(users).length;
+        console.log(length , " are no. of online users");
+        socket.broadcast.emit('user-joined-msg',{newuser:name , userCount:length});
+        socket.emit('prev-count',length);
+        // yeah sabko msg bhejhega ki new user aaya hai // the name 'user-joined' is a custom name not predefine
+    })
+
+    socket.on('send',message =>{
+        socket.broadcast.emit('receive',{message:message , Username : users[socket.id]})
+    })
+
+    socket.on('disconnect' , message=>{
+           var length = Object.keys(users).length;
+        console.log(length , " are no. of online users");
+        length--;
+        socket.broadcast.emit('left-msg',{leftUserName :users[socket.id] , userCount:length });
+        delete users[socket.id];  
+    })
+
+})
---
+++ js/client.js
@@ -0,0 +1,67 @@
+const socket = io("http://localhost:8000", { transports: ["websocket"] });
+
+const form = document.getElementById('send-container');
+const inputmsg = document.getElementById('input');
+const messagecontainer = document.querySelector('.messages');
+const usercount = document.getElementById('userCount');
+var audio = new Audio('tone.m4a');
+const userName = prompt("Enter Your name");
+socket.emit('new-user-joined',userName);
+const change_count =(count)=>{
+    usercount.innerText=`${count}`;
+}
+socket.on('prev-count',len=>{
+
+    change_count(len);
+})
+let count =0;
+
+
+const append_userjoined =(message , position)=>{
+    const newmsg = document.createElement('div');
+    newmsg.innerText=message;
+    newmsg.classList.add('msg2');
+    newmsg.classList.add(position);
+    messagecontainer.append(newmsg);
+   
+}
+const append_userleft =(message , position)=>{
+    const newmsg = document.createElement('div');
+    newmsg.innerText=message;
+    newmsg.classList.add('msg3');
+    newmsg.classList.add(position);
+    messagecontainer.append(newmsg);
+    console.log("dbks");
+}
+const append =(message , position)=>{
+    const newmsg = document.createElement('div');
+    newmsg.innerText=message;
+    newmsg.classList.add('msg');
+    newmsg.classList.add(position);
+    messagecontainer.append(newmsg);
+    console.log("dbks");
+    audio.play();
+}
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = input.value;
+    append(`You : ${message}`,'right');
+    socket.emit('send',`${message}`);
+    input.value="";
+})
+socket.on('user-joined-msg',data=>{
+
+    append_userjoined(`${data.newuser} has joined the chat`,'left');
+    change_count(data.userCount);
+})
+socket.on('receive',data=>{
+    
+    append(`${data.Username} : ${data.message}`,'left');
+})
+socket.on('left-msg',data =>{
+    append_userleft(`${data.leftUserName} has left the chat`,'left');
+    change_count(data.userCount);
+
+   
+})
+
